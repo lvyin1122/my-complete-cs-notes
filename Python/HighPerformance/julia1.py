946deec8546f053105bacdff6a571715f5cb329c
lvyin1122 <khiwhyzqd@outlook.com>
2020-08-06 19:22:07 +0800

    update

--- Python/HighPerformance/julia1.py
+++ Python/HighPerformance/julia1.py
@@ -1,5 +1,17 @@
 """Julia set generator without optional PIL-based image drawing"""
 import time
+# defining a decorator
+from functools import wraps
+
+def timefn(fn):
+    @wraps(fn)
+    def measure_time(*args, **kwargs):
+        t1 = time.time()
+        result = fn(*args, **kwargs)
+        t2 = time.time()
+        print('@timefn: ' + fn.__name__ + " took " + str(t2 - t1) + " seconds")
+        return result
+    return measure_time
 
 # area of complex space to investigate
 x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
@@ -31,15 +43,34 @@ def calc_pure_python(desired_width, max_iterations):
             zs.append(complex(xcoord, ycoord))
             cs.append(complex(c_real, c_imag))
 
-    print "Length of x:", len(x)
-    print "Total elements:", len(zs)
-    start_time = time.time()
+    print ("Length of x:", len(x))
+    print ("Total elements:", len(zs))
+    # start_time = time.time()
     output = calculate_z_serial_purepython(max_iterations, zs, cs)
-    end_time = time.time()
-    secs = end_time - start_time
-    print calculate_z_serial_purepython.func_name + " took", secs, "seconds"
+    # end_time = time.time()
+    # secs = end_time - start_time
+    # print (calculate_z_serial_purepython.__name__ + " took", secs, "seconds")
     
     # This sum is expected for a 1000^2 grid with 300 iterations.
     # It catches minor errors we might introduce when we're
     # working on a fixed set of inputs.
     assert sum(output) == 33219980
+
+@timefn
+def calculate_z_serial_purepython(maxiter, zs, cs):
+    """Calculate output list using Julia update rule"""
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while abs(z) < 2 and n < maxiter:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
+
+if __name__ == "__main__":
+    # Calculate the Julia set using a pure Python solution with
+    # reasonable defaults for a laptop
+    calc_pure_python(desired_width=1000, max_iterations=300)
