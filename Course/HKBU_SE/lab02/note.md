8481444faed1e8aae9f07c676ff0bc073292b0df
lvyin1122 <khiwhyzqd@outlook.com>
2020-09-28 17:38:35 +0800

    update

---
+++ Course/HKBU_SE/lab02/note.md
@@ -0,0 +1,11 @@
+## Form Elements
+
+`<input>` with different types
+
+text, number, url, date, checkbox
+
+`<select>` to give a combo box for selection `<option>`
+
+### Input Validation
+
+required, max, min
---
+++ Course/HKBU_SE/lab03/routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+
+var loki = require('lokijs');
+
+var db = new loki('data.json', {
+    autoload: true,
+    autoloadCallback: databaseInitialize,
+    autosave: true,
+    autosaveInterval: 4000
+});
+
+// implement the autoloadback referenced in loki constructor
+function databaseInitialize() {
+    var bookings = db.getCollection("bookings");
+    if (bookings === null) {
+        bookings = db.addCollection("bookings");
+    }
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Expresso' });
+});
+
+/* Handle the Form */
+router.post('/form', function (req, res) {
+
+    var response = {
+        header: req.headers,
+        body: req.body
+    };
+
+    req.body.numTickets = parseInt(req.body.numTickets);
+	
+    db.getCollection("bookings").insert(req.body);
+
+    res.json(response);    
+});
+
+/* Display all Bookings */
+router.get('/bookings', function (req, res) {
+
+    let result = db.getCollection("bookings").find();
+
+    res.render('bookings', { bookings: result });
+
+});
+
+module.exports = router;
